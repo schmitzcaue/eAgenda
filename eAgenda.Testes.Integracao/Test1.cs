b0f8e5514a8864848c5ea7a0e79449af043d9640
caues <caues@SCHMITZNOT>
2025-10-18 17:56:50 -0300

    Iniciada implementação de Testes de Integração de Contato

---
+++ eAgenda.Testes.Integracao/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace eAgenda.Testes.Integracao
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
