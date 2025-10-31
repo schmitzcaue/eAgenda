3d807c6fc3c53971bedce2d60696d6591aeb7c90
caues <caues@SCHMITZNOT>
2025-10-30 21:06:57 -0300

    Adicionado relacionamento entre entidades e usuários

--- eAgenda.Dominio/Compartilhado/EntidadeBase.cs
+++ eAgenda.Dominio/Compartilhado/EntidadeBase.cs
@@ -1,7 +1,11 @@
-﻿namespace eAgenda.Dominio.Compartilhado;
+﻿using eAgenda.Dominio.ModuloAutenticacao;
+
+namespace eAgenda.Dominio.Compartilhado;
 
 public abstract class EntidadeBase<T>
 {
+    public Guid UsuarioId { get; set; }
+    public Usuario? Usuario { get; set; }
     public Guid Id { get; set; }
 
     public abstract void AtualizarRegistro(T registroEditado);
